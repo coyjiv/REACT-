d507d1c23f5b15895e9aa6c48b974371c5c9acd2
Даниил Щербаков <djdaniil46@gmail.com>
2021-07-15 14:58:09 +0300

    initial commit

--- 1/homework-1/src/App.js
+++ 1/homework-1/src/App.js
@@ -10,13 +10,13 @@ class App extends React.Component{
             isSecondModalOpen:false,
         }
     }
-    handleFirstModalOpen(){
+    handleFirstModalOpen=()=>{
         this.setState((state)=>({
             ...state,
             isFirstModalOpen: !state.isFirstModalOpen
         }))
     }
-    handleSecondModalOpen(){
+    handleSecondModalOpen=()=>{
         this.setState((state)=>({
             ...state,
             isSecondModalOpen: !state.isSecondModalOpen
@@ -26,11 +26,23 @@ class App extends React.Component{
       return (
           <div className="App">
               <div className="btn-wrapper">
-                  <Button text="Open first modal" onClick={this.handleFirstModalOpen.bind(this)} backgroundColor="#B7F564" className="btn-open-modal"/>
-                  <Button text="Open second modal" onClick={this.handleSecondModalOpen.bind(this)} backgroundColor="#F52F15" className="btn-open-modal"/>
+                  <Button text="Open first modal" onClick={this.handleFirstModalOpen} backgroundColor="#B7F564" className="btn-open-modal"/>
+                  <Button text="Open second modal" onClick={this.handleSecondModalOpen} backgroundColor="#F52F15" className="btn-open-modal"/>
+                  <Modal isOpen={this.state.isFirstModalOpen} onCancel={()=>this.handleFirstModalOpen} header="Do you want to delete this file?" closeButton={false} text="Once you delete this file, it won’t be possible to undo this action. Are you sure you want to delete it?" actions={
+                  <>
+                      <h2>!!!!!!</h2>
+                      <Button text="Ok" className="btn-ok" onClick={this.handleFirstModalOpen}/>
+                      <Button text="Cancel" className="btn-cancel" onClick={this.handleFirstModalOpen}/>
+                      </>
+                  }/>
+                  <Modal isOpen={this.state.isSecondModalOpen} onCancel={()=>this.handleSecondModalOpen} header="Do you want to upload this file?" closeButton={true} text="something else" actions={
+                      <>
+                      <h2>Ogo</h2>
+                      <Button text="Ok" className="btn-ok" onClick={this.handleSecondModalOpen}/>
+                      <Button text="Cancel" className="btn-cancel" onClick={this.handleSecondModalOpen}/>
+                      </>
+                  }/>
               </div>
-              <Modal isOpen={this.state.isFirstModalOpen} onCancel={()=>this.handleFirstModalOpen()} header="Do you want to delete this file?" closeButton="true" text="Once you delete this file, it won’t be possible to undo this action. Are you sure you want to delete it?" actions={<h2>!!!!!!</h2>}/>
-              <Modal isOpen={this.state.isSecondModalOpen} onCancel={()=>this.handleSecondModalOpen()} header="Do you want to upload this file?" closeButton="true" text="something else" actions={<h2>Ogo</h2>}/>
           </div>
       );
   }
--- 1/homework-1/src/Components/Modal/Modal.jsx
+++ 1/homework-1/src/Components/Modal/Modal.jsx
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
 import "./Modal.scss"
-import Button from "../Button/Button";
+import cross from "./cross.png";
 class Modal extends Component {
     render() {
         return (
             <>
                 {this.props.isOpen &&
-                <div className="modalGlobal">
-                <div className="modalBody">
-                    <p className="modalHeading">{this.props.header}</p>
-                <p>{this.props.text}</p>
+                <div className="modal">
+                <div className="modal__content">
+                    <h1>{this.props.header}
+                        { this.props.closeButton ? <img src={cross} alt="cross" width="22px" height="22px" onClick={this.props.onCancel}/> : null}
+                    </h1>
+                    <p>{this.props.text}</p>
                     {this.props.actions}
                 </div>
-                <Button text="Ok" className="btn-ok" onClick={this.props.onClick}/>
-                <Button text="Cancel" className="btn-cancel" onClick={this.props.onCancel}/>
-
             </div>
                 }
             </>
